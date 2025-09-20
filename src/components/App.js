cec42cf6899b4fd70bfe48fdb23f6ae4bf6faa0c
Yash708262 <yash708262@gmail.com>
2025-09-20 13:53:38 +0530

    dfe

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import Heading from './Heaing.js';
+import Heading from './Heading.js';
 import InputQuery from './InputQuery.js';
 import SubHeading from './SubHeading.js';
 import SubmitButton from './SubmitButton.js';
